8de61680da2f4be8c7a2dd07681df8221d6b3cdc
Vikash7080 <sengarvikash256@gmail.com>
2025-07-11 22:20:18 +0530

    thought process of APIs

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -3,20 +3,22 @@ const connectionRequestSchema = new mongoose.Schema (
     {
         fromUserId:{
             type:mongoose.Schema.Types.ObjectId,
+            ref:"User",//reference to the User collection
             required:true,
         },
         toUserId:{
             type:mongoose.Schema.Types.ObjectId,
+            ref:"User",
             required:true,
         },
         status :{
             type:String,
             required:true,
-            enum:{
-                values:["ignore","interested","accepeted ","rejected"],
-                message: `{VALUE} is incorrect status type`,
-
-            },
+         enum: {
+  values: ["ignore", "interested", "accepted", "rejected"],
+  message: `{VALUE} is an incorrect status type`,
+}
+,
         },
 
 },
@@ -30,7 +32,7 @@ connectionRequestSchema.index({fromUserId:1,toUserId:1});
 
 
 //pre hook kund of a middleware that is used to check whether that touser id and fromuser id is same if it same then give res..
-connectionRequestSchema.pre("save", function () {
+connectionRequestSchema.pre("save", function (next) {
   const connectionRequest = this;
 
   // Check if the fromUserId is same as toUserId
