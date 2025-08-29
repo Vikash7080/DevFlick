414848005b8e4e24ab6c423f2da861d1faaaf8a6
Vikash7080 <sengarvikash256@gmail.com>
2025-08-29 22:12:54 +0530

    some others

--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,8 +1,14 @@
+require("dotenv").config(); // make sure envs load first
+
 const Razorpay = require("razorpay");
 
+// // Debug log
+// console.log("Razorpay key:", process.env.RAZORPAY_KEY_ID);
+// console.log("Razorpay secret:", process.env.RAZORPAY_KEY_SECRET);
+
 const instance = new Razorpay({
-  key_id:process.env.RAZORPAY_KEY_ID,
-  key_secret:process.env.RAZORPAY_KEY_SECRET,
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
+
 module.exports = instance;
- 
